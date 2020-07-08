caa5eb631972e73d4204eab1e0c3f42fd58b069e
qu <15010965776@163.com>
2020-07-09 00:04:09 +0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main/java/jenkins/controller/Controller.java
+++ src/main/java/jenkins/controller/Controller.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/jenkins")
+@RequestMapping("/myjenkins")
 public class Controller {
 
     @RequestMapping("/singletest1")
-    public String test1(){
+    public String tesssst1(){
+        return "this is not jenkins dddddddd"+"ssssssssssssssssss";
+    }
+
+    @RequestMapping("/singletest1")
+    public String test2(){
         return "this is jenkins singletest1";
     }
 }
